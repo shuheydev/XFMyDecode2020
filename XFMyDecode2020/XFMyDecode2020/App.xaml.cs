13014ff070833970dd7b37ee847a04cf14c81959
Shuhey <shuhey.dev@gmail.com>
2020-06-19 00:16:16 +0900

    Add project files.

---
+++ XFMyDecode2020/XFMyDecode2020/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFMyDecode2020
+{
+    public partial class App : Application
+    {
+        public App(Page startupPage)
+        {
+            InitializeComponent();
+
+            MainPage = startupPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
