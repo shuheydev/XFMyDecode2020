dbf0615cd48ee7cc209fdc9cb2221a74703200b3
Shuhey <shuhey.dev@gmail.com>
2020-06-20 06:10:16 +0900

    インクリメンタルなフィルタリングまでOk

--- XFMyDecode2020/XFMyDecode2020/ViewModels/SessionListViewModel.cs
+++ XFMyDecode2020/XFMyDecode2020/ViewModels/SessionListViewModel.cs
@@ -26,7 +26,49 @@ namespace XFMyDecode2020.ViewModels
             set => SetProperty(ref _sessions, value);
         }
 
+        private ObservableRangeCollection<SessionGroup> _groupedSessions;
+        public ObservableRangeCollection<SessionGroup> GroupedSessions
+        {
+            get => _groupedSessions;
+            set => SetProperty(ref _groupedSessions, value);
+        }
+
+        private string _searchString;
+        public string SearchString
+        {
+            get => _searchString;
+            set
+            {
+                SetProperty(ref _searchString, value);
+
+                var searchWords = Regex.Split(this.SearchString, @"\s+?").Where(w => !string.IsNullOrEmpty(w)).ToList();
+
+                //Let's filtering
+                var filteredSessions = Sessions.Where(s =>
+                {
+                    string target = string.Join(" ", new[] {
+                        s.SessionTitle,
+                        s.SessionDetails,
+                        s.SessionID,
+                        s.MainSpeaker.Company,
+                        s.MainSpeaker.Name,
+                        string.Join(" ", s.SubSpeakerList.Select(sub => $"{sub.Speaker.Company} {sub.Speaker.Name}")) }
+                    );
+
+                    return Check(target, searchWords);
+                }).ToList();
+
+                this.GroupedSessions.Clear();
+                this.GroupedSessions.AddRange(filteredSessions.GroupBy(s => s.TrackID)
+                                      .Select(g => new SessionGroup(g.Key, g.FirstOrDefault()?.TrackName, g.ToList())));
+            }
+        }
+
         public AsyncCommand<string> ShowSessionDetailsCommand { get; }
+        private async Task ShowSessionDetails(string sessionId)
+        {
+            await Shell.Current.GoToAsync($"sessionDetails?sessionId={sessionId}");
+        }
 
         private readonly IDataService _dataService;
 
@@ -37,12 +79,7 @@ namespace XFMyDecode2020.ViewModels
             ShowSessionDetailsCommand = new AsyncCommand<string>(ShowSessionDetails);
         }
 
-        private async Task ShowSessionDetails(string sessionId)
-        {
-            await Shell.Current.GoToAsync($"sessionDetails?sessionId={sessionId}");
-        }
-
-        public async Task LoadSessions()
+        internal async Task LoadSessions()
         {
             if (this.Sessions != null)
                 return;
@@ -63,11 +100,32 @@ namespace XFMyDecode2020.ViewModels
             }
         }
 
-        private ObservableRangeCollection<SessionGroup> _groupedSessions;
-        public ObservableRangeCollection<SessionGroup> GroupedSessions
+
+
+        private readonly string excludePrefix = "-";
+        private bool Check(string target, IEnumerable<string> searchWords)
         {
-            get => _groupedSessions;
-            set => SetProperty(ref _groupedSessions, value);
+            foreach (var word in searchWords)
+            {
+                //-がついていた場合
+                if (word.StartsWith(excludePrefix))
+                {
+                    string w = word.Substring(excludePrefix.Length);
+                    if (target.Contains(w, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return false;
+                    }
+
+                    continue;
+                }
+
+                if (!target.Contains(word, StringComparison.OrdinalIgnoreCase))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 
