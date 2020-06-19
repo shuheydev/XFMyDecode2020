f6d0fbd6306216f2253f34314fff8226d8e949e5
Shuhey <shuhey.dev@gmail.com>
2020-06-20 05:19:53 +0900

    スクロールに応じた検索ボックスの出し入れOK

--- XFMyDecode2020/XFMyDecode2020/Views/SessionListPage.xaml.cs
+++ XFMyDecode2020/XFMyDecode2020/Views/SessionListPage.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reactive.Linq;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -13,17 +11,61 @@ namespace XFMyDecode2020.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SessionListPage : ContentPage
     {
+        private string _message;
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
         private SessionListViewModel _viewModel;
         public SessionListPage()
         {
             InitializeComponent();
             this.BindingContext = _viewModel = Startup.ServiceProvider.GetService<SessionListViewModel>();
+
+            SetHeaderBehaviorByScroll();
         }
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
             await _viewModel.LoadSessions();
+        }
+
+        private readonly double _slideToggleYPosition = 110;
+        private void SetHeaderBehaviorByScroll()
+        {
+            //ヘッダーを隠す側の動作
+            var hide = Observable.FromEventPattern<ItemsViewScrolledEventArgs>(CollectionsView_Sessions, nameof(CollectionsView_Sessions.Scrolled));
+            hide.Where(x => x.EventArgs.VerticalDelta >= 0 && x.EventArgs.VerticalOffset >= _slideToggleYPosition)
+                .Repeat()
+                .Subscribe(x =>
+                {
+                    double nextTransY = frame_Header.TranslationY - x.EventArgs.VerticalDelta;
+
+                    nextTransY = Math.Max(-(frame_Header.Height + frame_Header.Y), nextTransY);
+                    frame_Header.TranslationY = nextTransY;
+                });
+
+            //ヘッダーを表示する側の動作
+            var show = Observable.FromEventPattern<ItemsViewScrolledEventArgs>(CollectionsView_Sessions, nameof(CollectionsView_Sessions.Scrolled));
+            show.Where(x => x.EventArgs.VerticalDelta < 0)
+                .Repeat()
+                .Subscribe(x =>
+                {
+                    double nextTransY = frame_Header.TranslationY - x.EventArgs.VerticalDelta;
+                    if (nextTransY > 0)
+                    {
+                        nextTransY = 0;
+                    }
+
+                    frame_Header.TranslationY = nextTransY;
+                });
         }
     }
 }
